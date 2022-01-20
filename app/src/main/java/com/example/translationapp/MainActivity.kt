bf062a21df581e79b4c63d17c49a775e77b2a16d
KevinSong0428 <ks5138@nyu.edu>
2022-01-20 18:07:04 -0500

    small update

--- app/src/main/java/com/example/translationapp/MainActivity.kt
+++ app/src/main/java/com/example/translationapp/MainActivity.kt
@@ -10,128 +10,137 @@ import android.speech.SpeechRecognizer
 import android.view.View
 import android.widget.*
 import androidx.core.content.ContextCompat
-import java.util.*
-import java.util.jar.Manifest
+import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
 
     companion object
     {
-        //private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.INTERNET)
-        private val REQUEST_CODE_PERMISSIONS = 1
+        private val REQUEST_CODE_PERMISSIONS = 100
         private var inputLanguage = "en_US"
         private var outputLanguage = "en_US"
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //access to items of the list
-        //val input_languages = resources.getStringArray(R.array.Input_Language)
-        //val output_languages = resources.getStringArray(R.array.Output_Language)
+        val btn = findViewById<Button>(R.id.btn)
 
-        val input_Adapter = ArrayAdapter.createFromResource(this, R.array.Input_Language, android.R.layout.simple_spinner_item)
-        val output_Adapter = ArrayAdapter.createFromResource(this, R.array.Output_Language, android.R.layout.simple_spinner_item)
-
-        input_Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        output_Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
+        // INPUT SPINNER SET UP
         val inSpinner = findViewById<Spinner>(R.id.inputLang)
-        val outSpinner = findViewById<Spinner>(R.id.outputLang)
+        // access to items of the list (in this case, languages)
+        val inputLanguages = resources.getStringArray(R.array.languages)
+        val inputAdapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item)
+        // set layout for choices to appear
+        inputAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // setSelection(0); <-- this will make default options for spinners
+        inSpinner.setSelection(0)
+        // set adapter to inputSpinner
+        inSpinner.adapter = inputAdapter
 
-        inSpinner.adapter = input_Adapter
-        outSpinner.adapter = output_Adapter
-
-/*        if (allPermissionsGranted())
-        {
-            startInput()
-        }*/
-
-        //input spinner selection
+        // input spinner selection
         inSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
                 // An item was selected. You can retrieve the selected item using parent.getItemAtPosition(pos)
-                when(parent.getItemAtPosition(pos)) {
+
+                when(inSpinner.getItemAtPosition(pos)) {
                     "English" -> inputLanguage = "en_US"
                     "廣東話" -> inputLanguage = "zh-HK"
                     "普通话" -> inputLanguage = "zh-Hans"
                     "Korean" -> inputLanguage = "Kor"
                 }
-            }
 
+                // print selected language
+                Toast.makeText(this@MainActivity,"Input language selected: " + inputLanguages[pos], Toast.LENGTH_SHORT).show()
+            }
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // Another interface callback
                 Toast.makeText(Activity(),"No input language selected.", Toast.LENGTH_SHORT).show()
             }
         }
 
-        //output spinner selection
+        // OUTPUT SPINNER SET UP
+        val outSpinner = findViewById<Spinner>(R.id.outputLang)
+        val outputLanguages = resources.getStringArray(R.array.languages)
+        val outputAdapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item)
+        outputAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        outSpinner.setSelection(0)
+        outSpinner.adapter = outputAdapter
+
+        /*
+        // output spinner selection
         outSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
-                when(parent.getItemAtPosition(pos)) {
+
+                when(outSpinner.getItemAtPosition(pos)) {
                     "English" -> outputLanguage = "en_US"
                     "廣東話" -> outputLanguage = "zh-HK"
                     "普通话" -> outputLanguage = "zh-Hans"
                     "Korean" -> outputLanguage = "Kor"
                 }
+                // print selected language
+                Toast.makeText(Activity(),"Output language selected: " + outputLanguages[pos], Toast.LENGTH_SHORT).show()
             }
-
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // Another interface callback
                 Toast.makeText(Activity(),"No output language selected.", Toast.LENGTH_SHORT).show()
             }
         }
 
-    }
-    //In onItemSelected method add this code sp.setSelection(0); <-- this will make default options for spinners
+         */
 
-    private fun startInput()
-    {
-        //will listen to user and go from speech to text
-        if (!SpeechRecognizer.isRecognitionAvailable(this)){
-            Toast.makeText(this, "Speech Recognition is not available", Toast.LENGTH_SHORT).show()
-        } else {
-            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH)
+        btn.setOnClickListener {
+            startInput()
         }
 
     }
 
+
+    private fun startInput()
+    {
+        val int = Intent(RecognizerIntent.ACTION_GET_LANGUAGE_DETAILS)
+        print(int)
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, inputLanguage)
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi, speak something")
+        try {
+            // if there is no error, show dialog
+            startActivityForResult(intent, REQUEST_CODE_PERMISSIONS)
+        } catch (e: Exception){
+            // if there is any error, get error message and show in Toast
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            REQUEST_CODE_PERMISSIONS -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    // get text from result
+                    val text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+
+                    // set the text to textView
+                    print(text)
+
+                }
+            }
+        }
+    }
+
     private fun startOutput()
     {
-        //cal translate, get string back and return back speech
+        // call translate, get string back and return back speech
 
     }
 
     private fun translate()
     {
-        //Translate ONLY if necessary aka when languages are different
+        // Translate ONLY if necessary aka when languages are different
 
     }
-
-
-/*
-    //PERMISSIONS - BEGIN
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startInput()
-            } else {
-                Toast.makeText(this, "Permission for microphone was denied." +
-                        "Please change in the settings.", Toast.LENGTH_LONG).show()
-                finish()
-            }
-        }
-    }
-    //PERMISSIONS - END
-    */
 
 }
